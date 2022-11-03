be9de64cd9c755998c6c20ba66249e1df56d34d2
krisbobo <kiyongbohbo@gmail.com>
2022-11-03 21:29:42 +0100

    Add function to iterate over tasks array

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('body');
-  
-    return element;
+const tasks = [
+  {
+    description: 'Commit to GitHub',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Solve a challenge on hackerrank',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Read a novel',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Watch a movie',
+    completed: false,
+    index: 3,
+  },
+];
+
+const info = () => {
+  const taskContent = document.querySelector('.list-items');
+  for (let i = 0; i < tasks.length; i++) {
+    const element = document.createElement('li');
+    element.classList.add('list-info');
+    element.innerHTML = `
+    <input type="checkbox" name="${tasks.index}" id="${tasks.index}" title="Check">
+    <label for="${tasks.index}" contentEditable="true">${tasks[i].description}</label>
+    <i class="material-icons">more_vert</i>
+    `;
+    taskContent.appendChild(element);
   }
-  
-  document.body.appendChild(component());
+};
+
+info();

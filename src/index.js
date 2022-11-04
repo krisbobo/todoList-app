d17ab7ff29f151daf88becde6dfb2718789f8f5f
krisbobo <kiyongbohbo@gmail.com>
2022-11-04 21:44:26 +0100

    Import files from modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
 import './style.css';
+import events from './modules/add_delete';
+import taskToEdit from './modules/edit';
+
+events();
+taskToEdit();

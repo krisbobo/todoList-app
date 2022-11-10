18e2649cd8e883e38528a719c832dceb845dbe57
krisbobo <kiyongbohbo@gmail.com>
2022-11-10 16:01:39 +0100

    Add file to export

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,3 +2,4 @@
 export { default as AddItemsToList } from './add_to_list';
 export { default as RemoveItemFromList } from './remove_from_list';
 export { default as Store } from './storage';
+export { default as UpdateStatus } from './update';
